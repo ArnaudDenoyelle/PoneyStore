4996df55e18475ab340b13d253cb5c6dd816cb3a
Arnaud DENOYELLE <denoyelle.arnaud@gmail.com>
2016-11-06 10:44:00 +0100

    Ajout du backend.

---
+++ backend/src/main/java/com/denodev/backend/MyApplication.java
@@ -0,0 +1,18 @@
+package com.denodev.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Arnaud DENOYELLE
+ *
+ *         Le 06 nov. 2016
+ */
+@SpringBootApplication
+public class MyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyApplication.class);
+  }
+
+}
